7f74e16c21359b4511f17b3904993fcd32390220
PREDATORMK2 <anand.tiwari@spit.ac.in>
2024-06-26 16:01:14 +0530

    Initial commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key':'2129f20a66mshe8d88afbb457cc4p1c6e8ajsn73e226a8d23a',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '2129f20a66mshe8d88afbb457cc4p1c6e8ajsn73e226a8d23a',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
